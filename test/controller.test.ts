122dc8688cd76357bafc5ebd2f60c3f4271f681e
liuyuan <373005226@qq.com>
2021-12-30 13:16:52 +0800

    添加腾讯云触发器、添加jest别名

--- test/controller.test.ts
+++ test/controller.test.ts
@@ -1,30 +1,35 @@
-import { createFunctionApp, close, createHttpRequest } from '@midwayjs/mock';
-import { Framework, Application } from '@midwayjs/serverless-app';
+import { join } from 'path'
+import { createFunctionApp, close, createHttpRequest } from '@midwayjs/mock'
+import { Framework, Application } from '@midwayjs/serverless-app'
+import { createCMQEvent } from '@midwayjs/serverless-scf-trigger'
 
 describe('test/controller.test.ts', () => {
+    let app: Application
 
-  let app: Application;
+    beforeAll(async () => {
+        // create app
+        app = await createFunctionApp<Framework>(join(__dirname, '../'), {
+            initContext: createCMQEvent()
+        })
+    })
 
-  beforeAll(async () => {
-    // create app
-    app = await createFunctionApp<Framework>();
-  });
+    afterAll(async () => {
+        await close(app)
+    })
 
-  afterAll(async () => {
-    await close(app);
-  });
+    it('should get /', async () => {
+        // make request
+        const result = await createHttpRequest(app).get('/')
 
-  it('should get /', async () => {
-    // make request
-    const result = await createHttpRequest(app).get('/');
+        // use expect by jest
+        expect(result.status).toBe(200)
+        expect(result.text).toBe('Hello Midwayjs')
 
-    // use expect by jest
-    expect(result.status).toBe(200);
-    expect(result.text).toBe('Hello Midwayjs');
-    
-    const result2 = await createHttpRequest(app).get('/get').query({ name: 123 });
-    // or use assert
-    expect(result2.status).toBe(200);
-    expect(result2.body.name).toBe('123');
-  });
-});
+        // const result2 = await createHttpRequest(app)
+        //     .get('/get')
+        //     .query({ name: 123 });
+        // // or use assert
+        // expect(result2.status).toBe(200);
+        // expect(result2.body.name).toBe('123');
+    })
+})
